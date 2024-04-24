3b2883e44e0895478991c6f9dfa593af4d0a8a2a
asasigure <mkmokymh@gmail.com>
2024-04-25 00:50:00 +0900

    headerの作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 import { Noto_Serif_JP } from "next/font/google";
 import "./globals.css";
 
@@ -16,7 +17,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={notoSerifJP.className}>{children}</body>
+      <body className={notoSerifJP.className}>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="z-10 w-full max-w-5xl items-center justify-between flex">
+            <Image
+              src="/asasigure.svg"
+              alt="asasigure logo"
+              width={159}
+              height={42}
+              priority
+            />
+            <nav className="flex items-center font-mono">
+              <a
+                className="pointer-events-auto mx-4"
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                engineering
+              </a>
+              <a
+                className="pointer-events-auto mx-4"
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                photos/videos
+              </a>
+              <a
+                className="pointer-events-auto mx-4"
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                about
+              </a>
+            </nav>
+          </div>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
