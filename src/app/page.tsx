78eb6839004e81dc3593764bcc4d7b165166ca8b
asasigure <mkmokymh@gmail.com>
2024-04-27 01:31:25 +0900

    レスポンシブな写真リストに

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,88 @@ export default function Home() {
     <>
       <h1 className="mt-24 mb-10">お気に入りの写真</h1>
 
-      <div className="grid text-center w-full max-w-3xl grid-cols-3 gap-4">
-        <Image
-          src="/photos/moon.jpg"
-          alt="moon"
-          width={246}
-          height={246}
-          className="w-full"
-        />
+      <div className="grid text-center w-full max-w-5xl grid-cols-[repeat(auto-fill,minmax(256px,1fr))] gap-4">
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/sky.jpg"
+            fill
+            alt="moon"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/moist.jpg"
+            fill
+            alt="moist"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/moist.jpg"
+            fill
+            alt="moist"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/sky.jpg"
+            fill
+            alt="moon"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/moist.jpg"
+            fill
+            alt="moist"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/moist.jpg"
+            fill
+            alt="moist"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/sky.jpg"
+            fill
+            alt="moon"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/moist.jpg"
+            fill
+            alt="moist"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
+        <div className="relative before:content-[''] pb-[100%]">
+          <Image
+            src="/photos/moist.jpg"
+            fill
+            alt="moist"
+            sizes="(max-width: 768px) 100vw, 492px"
+            className="object-cover"
+          />
+        </div>
       </div>
     </>
   );
